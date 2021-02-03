67929c11b504a23268651f945c7a699533a636dc
seireitou <ausis@mail.com>
2021-02-02 20:52:33 -0600

    adds get_zipcode in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,12 @@ class Cli
 
 def welcome
     puts "Welcome to the weather cli app!"
+    self.get_zipcode 
+end
+
+def get_zipcode
+    puts "Please enter your zipcode"
+    input = gets.strip
 end
 
 
